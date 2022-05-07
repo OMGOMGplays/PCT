9863c7215c0d94f13b3220fe78daf5d1d118f33b
OMGOMGplays <loke@jenjen.se>
2022-05-07 22:14:05 +0200

    Initial Commit
    - First work on fubny gamemode!!!

---
+++ code/Game.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCT
+{
+	public partial class PCTGame : Game
+	{
+		public static PCTGame Instance {get; private set;}
+
+		public PCTGame() 
+		{
+			Instance = this;
+
+			if (IsServer) 
+			{
+				// _ = new PCTHud();
+			}
+		}
+
+		public override void ClientJoined( Client cl )
+		{
+			base.ClientJoined( cl );
+
+			var player = new PCTPlayer(cl);
+			player.Respawn();
+			
+			cl.Pawn = player;
+		}
+	}
+}
---
+++ code/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace PCT
+{
+	partial class PCTPlayer : Player
+	{
+		public Clothing.Container Clothing = new();
+
+		public PCTPlayer() 
+		{
+		}
+
+		public PCTPlayer(Client cl)
+		{
+			Clothing.LoadFromClient(cl);
+		}
+
+		public override void Respawn()
+		{
+			base.Respawn();
+
+			SetModel("models/citizen/citizen.vmdl");
+		
+			Controller = new WalkController();
+			Animator = new StandardPlayerAnimator();
+			CameraMode = new FixedCamera();
+
+			Clothing.DressEntity(this);
+		}
+	}
+}
